8b62ce14b00665adba0e35555bf083f077b09f26
Madao OfTheYear <Madao OfTheYear>
2017-02-17 15:35:19 -0800

    Re-organized main game logics to be found in GameEngine class
    Signed-off-by: Madao OfTheYear <Madao OfTheYear>

--- src/event/KeyHandler.java
+++ src/event/KeyHandler.java
@@ -1,12 +1,14 @@
 package event;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFW.glfwGetKey;
 import static org.lwjgl.opengl.GL11.glClearColor;
+
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
 
 public class KeyHandler {
 
 	long window;
+	GLFWWindowSizeCallback windowSizeCallback;
 	
 	public KeyHandler(long window) { 
 		this.window = window;
---
+++ src/game/Renderer.java
@@ -0,0 +1,28 @@
+package game;
+
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+
+public class Renderer {
+	
+	long window;
+
+    public Renderer(long window) {        
+    	this.window = window;
+    }
+    
+    public void init() throws Exception {
+		// Set the initial clear color
+		glClearColor(1.0f, 0.5f, 0.0f, 0.0f);
+    }
+
+    public void clear() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+    
+    public void render() {
+    	clear();
+    }
+}
